db098c58285fcec7794c790c4e9efae0534e375b
AlyshaSaad <alysaadberreta@gmail.com>
2026-03-04 16:53:48 -0500

    Completing Lab 4

--- lab4/ble-central/src/main.c
+++ lab4/ble-central/src/main.c
@@ -12,9 +12,9 @@
 #include <zephyr/sys/byteorder.h>
 
 #define LAB4_SERVICE_UUID_VAL \
-  BT_UUID_128_ENCODE(0x5253FF4B, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B4A)
+  BT_UUID_128_ENCODE(0x5253FF4B, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B07)
 #define LAB4_SERVICE_UUID_CONST_VAL \
-  BT_UUID_128_ENCODE(0x5253FF4C, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B4A)
+  BT_UUID_128_ENCODE(0x5253FF4D, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B07)
 
 #define LAB4_SERVICE_UUID BT_UUID_DECLARE_128(LAB4_SERVICE_UUID_VAL)
 #define LAB4_SERVICE_UUID_CONST BT_UUID_DECLARE_128(LAB4_SERVICE_UUID_CONST_VAL)
@@ -34,7 +34,7 @@ static uint8_t read_func(struct bt_conn *conn, uint8_t err,
 			       const void *data, uint16_t length)
 {
 	if (err) {
-		printk("read failed (err %d)\n", err);
+		// printk("read failed (err %d)\n", err);
 	}
 
 	uint8_t* buf = (uint8_t*) data;
@@ -48,7 +48,7 @@ static uint8_t read_func(struct bt_conn *conn, uint8_t err,
 		printk("Read: 0x%.8x\n", val);
 	}
 
-    // bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+    bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
 
 	return BT_GATT_ITER_STOP;
 }
@@ -61,21 +61,21 @@ static uint8_t discover_func(struct bt_conn *conn,
     char uuid_str[BT_UUID_STR_LEN];
 
 	if (!attr) {
-		printk("Discover complete\n");
+		// printk("Discover complete\n");
 		(void)memset(params, 0, sizeof(*params));
 		return BT_GATT_ITER_STOP;
 	}
 
-	printk("[ATTRIBUTE] handle %u\n", attr->handle);
+	// printk("[ATTRIBUTE] handle %u\n", attr->handle);
 
     bt_uuid_to_str(params->uuid, uuid_str, sizeof(uuid_str));
 
-    printk("UUID: %s\n", uuid_str);
+    // printk("UUID: %s\n", uuid_str);
 
-    printk("%d\n", bt_uuid_cmp(params->uuid, LAB4_SERVICE_UUID));
+    // printk("%d\n", bt_uuid_cmp(params->uuid, LAB4_SERVICE_UUID));
 
 	if (bt_uuid_cmp(params->uuid, LAB4_SERVICE_UUID) == 0) {
-		printk("Found service\n");
+		// printk("Found service\n");
 
 		discover_params.uuid = search_characteristic_uuid;
 		discover_params.start_handle = attr->handle + 1;
@@ -83,11 +83,11 @@ static uint8_t discover_func(struct bt_conn *conn,
 
 		err = bt_gatt_discover(conn, &discover_params);
 		if (err) {
-			printk("Discover failed (err %d)\n", err);
+			// printk("Discover failed (err %d)\n", err);
 		}
 	}
 	else if (bt_uuid_cmp(params->uuid, LAB4_SERVICE_UUID_CONST) == 0) {
-		printk("Found characteristic\n");
+		// printk("Found characteristic\n");
 
 		read_params.func = read_func;
 		read_params.handle_count = 1;
@@ -96,7 +96,7 @@ static uint8_t discover_func(struct bt_conn *conn,
 
 		err = bt_gatt_read(conn, &read_params);
 		if (err) {
-			printk("Read failed (err %d)\n", err);
+			// printk("Read failed (err %d)\n", err);
 		}
 	}
 
@@ -112,7 +112,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	if (conn_err) {
-		printk("Failed to connect to %s (%u)\n", addr, conn_err);
+		// printk("Failed to connect to %s (%u)\n", addr, conn_err);
 
 		bt_conn_unref(default_conn);
 		default_conn = NULL;
@@ -121,7 +121,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 		return;
 	}
 
-	printk("Connected: %s\n", addr);
+	// printk("Connected: %s\n", addr);
   
 	if (conn == default_conn) {
 		discover_params.uuid = search_service_uuid;
@@ -132,11 +132,11 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 
         bt_uuid_to_str(discover_params.uuid, uuid_str, sizeof(uuid_str));
 
-        printk("UUID: %s\n", uuid_str);
+        // printk("UUID: %s\n", uuid_str);
 
 		err = bt_gatt_discover(default_conn, &discover_params);
 		if (err) {
-			printk("Discover failed(err %d)\n", err);
+			// printk("Discover failed(err %d)\n", err);
 			return;
 		}
 	}
@@ -148,7 +148,7 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
+	// printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
 
 	if (default_conn != conn) {
 		return;
@@ -170,12 +170,12 @@ static bool ad_found(struct bt_data *data, void *user_data)
 {
 	bt_addr_le_t *addr = user_data;
 
-	printk("[AD]: %u data_len %u\n", data->type, data->data_len);
+	// printk("[AD]: %u data_len %u\n", data->type, data->data_len);
 
 	switch (data->type) {
 	case BT_DATA_UUID128_ALL:
 		if (data->data_len != 16) {
-			printk("AD malformed\n");
+			// printk("AD malformed\n");
 			return true;
 		}
 
@@ -185,18 +185,18 @@ static bool ad_found(struct bt_data *data, void *user_data)
 
 		bt_uuid_create(&uuid, data->data, 16);
 		if (bt_uuid_cmp(&uuid, LAB4_SERVICE_UUID) == 0) {
-			printk("Found matching advertisement\n");
+			// printk("Found matching advertisement\n");
 
 			err = bt_le_scan_stop();
 			if (err) {
-				printk("Stop LE scan failed (err %d)\n", err);
+				// printk("Stop LE scan failed (err %d)\n", err);
 				return false;
 			}
 
 			param = BT_LE_CONN_PARAM_DEFAULT;
 			err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, param, &default_conn);
 			if (err) {
-				printk("Create conn failed (err %d)\n", err);
+				// printk("Create conn failed (err %d)\n", err);
 				start_scan();
 			}
 		}
@@ -214,8 +214,8 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 	char dev[BT_ADDR_LE_STR_LEN];
 
 	bt_addr_le_to_str(addr, dev, sizeof(dev));
-	printk("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
-	       dev, type, ad->len, rssi);
+	// printk("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
+	    //    dev, type, ad->len, rssi);
 
 	// We're only interested in connectable devices.
 	if (type == BT_GAP_ADV_TYPE_SCAN_RSP) {
@@ -239,21 +239,21 @@ static void start_scan(void)
 
 	err = bt_le_scan_start(&scan_param, device_found);
 	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
+		// printk("Scanning failed to start (err %d)\n", err);
 		return;
 	}
 
-	printk("Scanning successfully started\n");
+	// printk("Scanning successfully started\n");
 }
 
 static void bt_ready(int err)
 {
 	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+		// printk("Bluetooth init failed (err %d)\n", err);
 		return;
 	}
 
-	printk("Bluetooth initialized\n");
+	// printk("Bluetooth initialized\n");
 
 	start_scan();
 }
@@ -265,7 +265,7 @@ int main(void)
 	err = bt_enable(bt_ready);
 
 	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+		// printk("Bluetooth init failed (err %d)\n", err);
 		return 0;
 	}
 }
--- lab4/ble-peripheral/src/main.c
+++ lab4/ble-peripheral/src/main.c
@@ -8,125 +8,164 @@
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/kernel.h>
 
+
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
 
+
 #define LAB4_SERVICE_UUID_VAL \
-  BT_UUID_128_ENCODE(0x5253FF4B, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B4A)
+ BT_UUID_128_ENCODE(0x5253FF4B, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B13)
 #define LAB4_SERVICE_UUID_CONST_VAL \
-  BT_UUID_128_ENCODE(0x5253FF4C, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B4A)
+ BT_UUID_128_ENCODE(0x5253FF13, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B13)
+#define LAB4_SERVICE_UUID_COUNTER_VAL \
+ BT_UUID_128_ENCODE(0x5253FF4D, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B13)
+
 
 #define LAB4_SERVICE_UUID BT_UUID_DECLARE_128(LAB4_SERVICE_UUID_VAL)
 #define LAB4_SERVICE_UUID_CONST BT_UUID_DECLARE_128(LAB4_SERVICE_UUID_CONST_VAL)
+#define LAB4_SERVICE_UUID_COUNTER BT_UUID_DECLARE_128(LAB4_SERVICE_UUID_COUNTER_VAL)
+
 
 static ssize_t const_read(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset);
 
+
 // Global value that saves state for the characteristic.
-uint32_t const_value = 0x1;
+uint32_t const_value = 0xc5ec4501;
+uint32_t value1 = 0x0;
+
 
 // Set up the advertisement data.
-#define DEVICE_NAME "EmbWIoTSensor"
+#define DEVICE_NAME "EmbWIoT_ASBMSIA_13"
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
+
 static const struct bt_data ad[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+   BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
 
+
 static const struct bt_data sd[] = {
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, LAB4_SERVICE_UUID_VAL),
+   BT_DATA_BYTES(BT_DATA_UUID128_ALL, LAB4_SERVICE_UUID_VAL),
 };
+
 
 // Setup the the service and characteristics.
 BT_GATT_SERVICE_DEFINE(lab4_service,
-	BT_GATT_PRIMARY_SERVICE(LAB4_SERVICE_UUID),
-	BT_GATT_CHARACTERISTIC(LAB4_SERVICE_UUID_CONST, BT_GATT_CHRC_READ,
-			       BT_GATT_PERM_READ, const_read, NULL, &const_value),
+   BT_GATT_PRIMARY_SERVICE(LAB4_SERVICE_UUID),
+   BT_GATT_CHARACTERISTIC(LAB4_SERVICE_UUID_CONST, BT_GATT_CHRC_READ,
+                  BT_GATT_PERM_READ, const_read, NULL, &const_value),
+   BT_GATT_CHARACTERISTIC(LAB4_SERVICE_UUID_COUNTER, BT_GATT_CHRC_READ,
+                  BT_GATT_PERM_READ, const_read, NULL, &value1)
 );
+
+
 
 
 // Callback when a client reads the characteristic.
 //
 // Documented under name "bt_gatt_attr_read_chrc()"
 static ssize_t const_read(struct bt_conn *conn,
-			                       const struct bt_gatt_attr *attr,
-								   void *buf,
-			                       uint16_t len,
-								   uint16_t offset)
+                                  const struct bt_gatt_attr *attr,
+                                  void *buf,
+                                  uint16_t len,
+                                  uint16_t offset)
 {
-	// The `user_data` corresponds to the pointer provided as the last "argument"
-	// to the `BT_GATT_CHARACTERISTIC` macro.
-	uint32_t *value = attr->user_data;
+   // The `user_data` corresponds to the pointer provided as the last "argument"
+   // to the `BT_GATT_CHARACTERISTIC` macro.
+   uint32_t *value = attr->user_data;
 
-	*value=0xc5ec4501;
 
-	// User helper function to encode the output data to send to
-	// the client.
-	return bt_gatt_attr_read(conn, attr, buf, len, offset, value, sizeof(*value));
+   // User helper function to encode the output data to send to
+   // the client.
+   ssize_t ret =bt_gatt_attr_read(conn, attr, buf, len, offset, value, sizeof(*value));
+
+
+   if (ret >= 0) {
+       if (value == &value1) {
+           value1++;   // Only increment this one
+       }
+   }
+
+
+   return ret;
 }
+
+
 
 
 // Setup callbacks when devices connect and disconnect.
 static void connected(struct bt_conn *conn, uint8_t err)
 {
-	if (err) {
-		printk("Connection failed (err 0x%02x)\n", err);
-	} else {
-		printk("Connected\n");
-	}
+   if (err) {
+       printk("Connection failed (err 0x%02x)\n", err);
+   } else {
+       printk("Connected\n");
+   }
 }
+
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	printk("Disconnected (reason 0x%02x)\n", reason);
+   printk("Disconnected (reason 0x%02x)\n", reason);
 }
+
 
 static void recycled_cb(void)
 {
-	printk("Connection object available from previous conn. Disconnect is complete!\n");
-    int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+   printk("Connection object available from previous conn. Disconnect is complete!\n");
+   int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
 
-	printk("Advertising successfully started\n");
+   if (err) {
+       printk("Advertising failed to start (err %d)\n", err);
+       return;
+   }
+
+
+   printk("Advertising successfully started\n");
 }
 
+
 BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-    .recycled = recycled_cb,
+   .connected = connected,
+   .disconnected = disconnected,
+   .recycled = recycled_cb,
 };
+
 
 static void bt_ready(int err)
 {
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return;
-	}
+   if (err) {
+       printk("Bluetooth init failed (err %d)\n", err);
+       return;
+   }
 
-	printk("Bluetooth initialized\n");
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
+   printk("Bluetooth initialized\n");
 
-	printk("Advertising successfully started\n");
+
+   err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+   if (err) {
+       printk("Advertising failed to start (err %d)\n", err);
+       return;
+   }
+
+
+   printk("Advertising successfully started\n");
 }
+
 
 int main(void)
 {
-	int err;
+   int err;
 
-	err = bt_enable(bt_ready);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return 0;
-	}
+
+   err = bt_enable(bt_ready);
+   if (err) {
+       printk("Bluetooth init failed (err %d)\n", err);
+       return 0;
+   }
 }
